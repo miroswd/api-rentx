7335b93fdf9863667f7820bc948b2502d7e355c6
miroswd <altamirsantos93@hotmail.com>
2022-02-24 23:04:24 -0300

    feat: implement liskov principle

---
+++ src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,20 @@
+import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class PostgresCategoriesRepository implements ICategoriesRepository {
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+  create({ name, description }: ICreateCategoryDTO): void {
+    console.log(name, description);
+  }
+  list(): Category[] {
+    return null;
+  }
+}
+
+export { PostgresCategoriesRepository };
