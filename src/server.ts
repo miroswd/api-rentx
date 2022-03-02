bcc52437f9670e8b9d884c980f377d53cccb2b05
miroswd <altamirsantos93@hotmail.com>
2022-03-01 21:17:20 -0300

    feat: connect with database usign typeorm

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import swaggerUi from "swagger-ui-express";
 
 import { router as routes } from "./routes";
 import swaggerFile from "./swagger.json";
+import "./database";
 
 const app = express();
 app.use(express.json());
