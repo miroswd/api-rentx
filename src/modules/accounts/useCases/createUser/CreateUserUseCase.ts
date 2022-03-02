05cba9ed86dd598ccfac11013d6cfa8964de2c58
miroswd <altamirsantos93@hotmail.com>
2022-03-02 13:27:23 -0300

    feat: create user table

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUsersDTO } from "../../dtos/ICreateUsersDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(data: ICreateUsersDTO): Promise<void> {
+    const { name, username, email, password, driver_license } = data;
+
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
