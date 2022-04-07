aa35da137da05ac481eb356c388c1cdf7ce37f25
miroswd <altamirsantos93@hotmail.com>
2022-04-07 14:33:48 -0300

    fix: remove host to exec server from root

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -18,12 +18,11 @@ getConnectionOptions().then((options) => {
 });
 */
 
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
